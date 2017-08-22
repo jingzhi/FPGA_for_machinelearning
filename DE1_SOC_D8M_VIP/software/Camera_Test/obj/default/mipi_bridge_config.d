ffe08ac38ab7a091a8777f0330257221e82b9c8d
Serena <jingzhi_li@outlook.com>
2017-08-22 12:15:57 +0100

    Add files via upload

---
+++ DE1_SOC_D8M_VIP/software/Camera_Test/obj/default/mipi_bridge_config.d
@@ -0,0 +1,73 @@
+obj/default/mipi_bridge_config.o: mipi_bridge_config.c I2C_core.h \
+ ../Camera_Test_bsp//HAL/inc/alt_types.h terasic_includes.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_llist.h \
+ ../Camera_Test_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_cache.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../Camera_Test_bsp//HAL/inc/io.h ../Camera_Test_bsp//HAL/inc/alt_types.h \
+ ../Camera_Test_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_irq.h \
+ ../Camera_Test_bsp//HAL/inc/nios2.h ../Camera_Test_bsp/system.h \
+ ../Camera_Test_bsp/linker.h ../Camera_Test_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_llist.h \
+ ../Camera_Test_bsp//HAL/inc/priv/alt_alarm.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../Camera_Test_bsp//drivers/inc/altera_avalon_timer.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_dev.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_warning.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_stdio.h mipi_bridge_config.h
+
+I2C_core.h:
+
+../Camera_Test_bsp//HAL/inc/alt_types.h:
+
+terasic_includes.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_llist.h:
+
+../Camera_Test_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_cache.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../Camera_Test_bsp//HAL/inc/io.h:
+
+../Camera_Test_bsp//HAL/inc/alt_types.h:
+
+../Camera_Test_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_irq.h:
+
+../Camera_Test_bsp//HAL/inc/nios2.h:
+
+../Camera_Test_bsp/system.h:
+
+../Camera_Test_bsp/linker.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_llist.h:
+
+../Camera_Test_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../Camera_Test_bsp//drivers/inc/altera_avalon_timer.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_dev.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_warning.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_stdio.h:
+
+mipi_bridge_config.h:
---
+++ DE1_SOC_D8M_VIP/software/Camera_Test/obj/default/queue.d
@@ -0,0 +1,71 @@
+obj/default/queue.o: queue.c terasic_includes.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash.h \
+ ../Camera_Test_bsp//HAL/inc/alt_types.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_dev.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_llist.h \
+ ../Camera_Test_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_cache.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h \
+ ../Camera_Test_bsp//HAL/inc/io.h ../Camera_Test_bsp//HAL/inc/alt_types.h \
+ ../Camera_Test_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_irq.h \
+ ../Camera_Test_bsp//HAL/inc/nios2.h ../Camera_Test_bsp/system.h \
+ ../Camera_Test_bsp/linker.h ../Camera_Test_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_llist.h \
+ ../Camera_Test_bsp//HAL/inc/priv/alt_alarm.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_timestamp.h \
+ ../Camera_Test_bsp//drivers/inc/altera_avalon_timer.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_dev.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_warning.h \
+ ../Camera_Test_bsp//HAL/inc/sys/alt_stdio.h queue.h
+
+terasic_includes.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash.h:
+
+../Camera_Test_bsp//HAL/inc/alt_types.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_dev.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_llist.h:
+
+../Camera_Test_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_cache.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_flash_types.h:
+
+../Camera_Test_bsp//HAL/inc/io.h:
+
+../Camera_Test_bsp//HAL/inc/alt_types.h:
+
+../Camera_Test_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_irq.h:
+
+../Camera_Test_bsp//HAL/inc/nios2.h:
+
+../Camera_Test_bsp/system.h:
+
+../Camera_Test_bsp/linker.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_llist.h:
+
+../Camera_Test_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../Camera_Test_bsp//drivers/inc/altera_avalon_timer.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_dev.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_warning.h:
+
+../Camera_Test_bsp//HAL/inc/sys/alt_stdio.h:
+
+queue.h:
